359aec5147d83405d1b259ff1677aa2878784d4d
Scorpion950 <yagaikwad12345@gmail.com>
2026-08-19 23:03:56 +0530

    created a global chat backend using websocket

--- week 16 --2/src/index.ts
+++ week 16 --2/src/index.ts
@@ -1,8 +1,31 @@
-import {WebSocketServer} from 'ws'
+import {WebSocketServer, WebSocket} from 'ws'
 
 const wss = new WebSocketServer({port:8080});
 
+let userCount = 0;
+
+let allSockets: WebSocket[] = [];
+
 wss.on("connection", (socket) => {
 
-    
-})
+    allSockets.push(socket);
+
+    userCount = userCount + 1;
+
+    console.log("user Connected #" + userCount);
+
+    socket.on("message", (event) => {
+
+        console.log("message received " + event.toString())
+
+        for (let i = 0; i < allSockets.length; i++) {
+
+            const s = allSockets[i];
+        if (s) {
+            s.send(event.toString() + ": sent from server");
+        }
+        }
+
+    });
+
+});
