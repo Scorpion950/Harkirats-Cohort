ca8e4f546a56df46fe22c89782aca1cce639422e
Scorpion950 <yagaikwad12345@gmail.com>
2026-08-28 12:37:23 +0530

    initialised week-20

---
+++ week 20 --1/week_20/app/signin/page.tsx
@@ -0,0 +1,10 @@
+export default function (){
+    return (
+        <div>
+            SignIn
+            <input type = "text"></input>
+            <input type = "text"></input>
+            <button>Sign in</button>
+        </div>
+    )
+}
