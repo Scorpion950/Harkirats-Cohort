901ed6a3a707d337126ef665d758c3db1e4fbd36
Scorpion950 <yagaikwad12345@gmail.com>
2026-09-05 01:07:41 +0530

    edited home page added login and logout

--- week 20 --2/nextauth/app/page.tsx
+++ week 20 --2/nextauth/app/page.tsx
@@ -1,12 +1,28 @@
 "use client";
-import {useSession} from "next-auth/react";
+
+import { SessionProvider, useSession } from "next-auth/react";
+import{ signIn, signOut } from "next-auth/react";
 
 export default function Home() {
+  return (
+    <div>
+      <SessionProvider>
+        <RealHome />
+      </SessionProvider>
+    </div>
+  );
+}
+
+function RealHome() {
   const session = useSession();
 
   return (
     <div>
-      Hello World
+      
+      {session.status === "authenticated" && <button onClick={() =>signOut()}>Logout</button>}
+
+      {session.status === "unauthenticated" && <button onClick={() =>signIn()}>Signin</button>}
+
     </div>
   );
 }
