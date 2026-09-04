1ab8f0833c68e01260d9db75d952b0816901eae1
Scorpion950 <yagaikwad12345@gmail.com>
2026-09-05 01:57:55 +0530

    completed week 20

--- week 20 --2/nextauth/app/page.tsx
+++ week 20 --2/nextauth/app/page.tsx
@@ -1,28 +1,43 @@
-"use client";
+// "use client";
 
-import { SessionProvider, useSession } from "next-auth/react";
-import{ signIn, signOut } from "next-auth/react";
+import {getServerSession} from "next-auth";
 
-export default function Home() {
+// import { SessionProvider, useSession } from "next-auth/react";
+// import{ signIn, signOut } from "next-auth/react";
+
+//second method to do thios sign in function - 
+
+export default async function Home(){
+  const session = await getServerSession();
+
+
   return (
     <div>
-      <SessionProvider>
-        <RealHome />
-      </SessionProvider>
+    {JSON.stringify(session)}
     </div>
   );
 }
 
-function RealHome() {
+// export default function Home() {
+//   return (
+//     <div>
+//       <SessionProvider>
+//         <RealHome />
+//       </SessionProvider>
+//     </div>
+//   );
+// }
+
+ /* function RealHome() {
   const session = useSession();
 
   return (
     <div>
-      
+
       {session.status === "authenticated" && <button onClick={() =>signOut()}>Logout</button>}
 
       {session.status === "unauthenticated" && <button onClick={() =>signIn()}>Signin</button>}
 
     </div>
   );
-}
+} */
