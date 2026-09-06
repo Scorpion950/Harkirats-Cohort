358ec5d7916c2849307871cff9d9e967410b212c
Scorpion950 <yagaikwad12345@gmail.com>
2026-09-06 14:04:03 +0530

    added a text input button and configured it

---
+++ week 21 --2/chat-app/apps/web/app/page.tsx
@@ -0,0 +1,26 @@
+import {TextInput} from "@repo/ui/text-input";
+
+export default function Home() {
+  return (
+
+    <div style ={{
+      height: "100vh",
+      width: "100vw",
+      background: "black",
+      display: "flex",
+      justifyContent:"center",
+      justifyItems: "center",
+    }}>
+  <div style = {{
+    display: "flex",
+    justifyContent:"center",
+    flexDirection:"column"
+  }
+  }>
+    <TextInput placholder = "roomCode"> </TextInput>
+      <input type = "text"></input>
+      <button>Join Room</button>
+    </div>
+  </div>
+  );
+}
